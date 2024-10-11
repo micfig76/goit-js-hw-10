fac33eee19bac7d27392aec6136476582e194e30
Michal Figiel <50012494+micfig76@users.noreply.github.com>
2024-10-11 11:42:43 +0200

    added flatpickr

---
+++ src/js/01-timer.js
@@ -0,0 +1,11 @@
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };
+
+flatpickr("#datetime-picker", {enableTime: true});
