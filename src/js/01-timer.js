560edbdda50e503c8e0de0b3da1948af02c17bc2
Michal Figiel <50012494+micfig76@users.noreply.github.com>
2024-10-11 12:05:43 +0200

    flatpckr adjustment, date submit handler

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -1,11 +1,19 @@
+
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-      console.log(selectedDates[0]);
+        console.log(selectedDates[0]);
     },
-  };
+};
 
-flatpickr("#datetime-picker", {enableTime: true});
+flatpickr("input#datetime-picker", options);
+
+document.querySelector("button[data-start]").addEventListener("click", () => {
+const value = document.querySelector("input#datetime-picker").value;
+const targetMs = new Date(value).getTime();
+console.log(targetMs)        
+});
+
